0f640799a5948c6866d52bbefd55c6d263003ae7
alaruqihwala <alaruqihwala@utexas.edu>
2024-02-13 21:44:08 -0600

    New Updates

---
+++ ThonnyPractice.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    myrem = i % 2
+    if myrem == 0 :
+        print(f"{i} is even!")
+    else:
+        print(f"{i} is odd!")
+        
+    
+    
---
+++ ThonnyPracticeWhile.py
@@ -0,0 +1,11 @@
+x = 0
+while x < 10 :
+    myrem = x % 2
+    if myrem == 0 :
+        print(f" {myrem} is even!")
+    else :
+        print(f" {myrem} is odd!")
+    x = x + 1
+   
+   
+    
